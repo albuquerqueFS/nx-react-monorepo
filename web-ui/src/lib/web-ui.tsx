67e5128d7985b765b1460fa1f445c6c5dc93ea48
Henrique Albuquerque <henrique.albuquerque@cantire.com>
2024-06-09 23:53:06 -0300

    setup

---
+++ web-ui/src/lib/web-ui.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledWebUi = styled.div`
+  color: pink;
+`;
+
+export function WebUi() {
+  return (
+    <StyledWebUi>
+      <h1>Welcome to WebUi!</h1>
+    </StyledWebUi>
+  );
+}
+
+export default WebUi;
